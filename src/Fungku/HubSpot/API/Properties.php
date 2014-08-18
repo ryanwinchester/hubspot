92137aab31be5ab4f011dcd1b24fc7bd6f04c723
Adam Engebretson <adam@enge.me>
2014-08-18 18:00:30 -0500

    Fixing Improper Class Call

--- src/Fungku/HubSpot/API/Properties.php
+++ src/Fungku/HubSpot/API/Properties.php
@@ -20,7 +20,7 @@ namespace Fungku\HubSpot\API;
 * License.
 */
 
-class Properties extends Baseclient{
+class Properties extends BaseClient{
 
 	protected $API_PATH = 'contacts';
 	protected $API_VERSION = 'v1';	
