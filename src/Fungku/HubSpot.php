9be240e8817eb592b4313e203519af57994a13ab
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2015-07-30 11:41:55 +0100

    Add connecttimeout + useragent

--- src/Fungku/HubSpot.php
+++ src/Fungku/HubSpot.php
@@ -29,36 +29,37 @@ class HubSpot {
      * @param string $hapikey
      * @param string $userAgent
      */
-    function __construct($hapikey = null, $userAgent = "haPiHP default UserAgent")
+    function __construct($hapikey = null, $connectTimeout, $userAgent = "haPiHP default UserAgent")
     {
         $this->hapikey = $hapikey;
+        $this->connectTimeout = $connectTimeout;
         $this->userAgent = $userAgent;
 
         if (is_null($this->hapikey))
             $this->hapikey = getenv('HUBSPOT_APIKEY');
     }
 
-    public function blog()          { return new Blog($this->hapikey); }
+    public function blog()          { return new Blog($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function contacts()      { return new Contacts($this->hapikey); }
+    public function contacts()      { return new Contacts($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function forms()         { return new Forms($this->hapikey); }
+    public function forms()         { return new Forms($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function keywords()      { return new Keywords($this->hapikey); }
+    public function keywords()      { return new Keywords($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function leadNurturing() { return new LeadNurturing($this->hapikey); }
+    public function leadNurturing() { return new LeadNurturing($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function leads()         { return new Leads($this->hapikey); }
+    public function leads()         { return new Leads($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function lists()         { return new Lists($this->hapikey); }
+    public function lists()         { return new Lists($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function marketPlace()   { return new MarketPlace($this->hapikey); }
+    public function marketPlace()   { return new MarketPlace($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function properties()    { return new Properties($this->hapikey); }
+    public function properties()    { return new Properties($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function settings()      { return new Settings($this->hapikey); }
+    public function settings()      { return new Settings($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function socialMedia()   { return new SocialMedia($this->hapikey); }
+    public function socialMedia()   { return new SocialMedia($this->hapikey, $this->connectTimeout, $this->userAgent); }
 
-    public function workflows()     { return new WorkFlows($this->hapikey); }
+    public function workflows()     { return new WorkFlows($this->hapikey, $this->connectTimeout, $this->userAgent); }
 }
